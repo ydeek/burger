5d9345292c468b9ca3a07d5b4cb1953491b1221f
Yazan Deek <ydeek91@yahoo.com>
2020-10-08 21:32:27 -0400

    declared dependencies

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 
+// Dependencies
 var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
