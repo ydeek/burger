40a35a77458d100d283e37f90e3688fc039d1f5a
Yazan Deek <ydeek91@yahoo.com>
2020-10-08 21:02:19 -0400

    code to add new burger to db

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -16,3 +16,11 @@ router.get("/", function (req, res) {
         res.render("index", hbsObject);
     });
 });
+
+// Add new burger to the db.
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
+        // Send back the ID of the new burger
+        res.json({ id: result.insertId });
+    });
+});
