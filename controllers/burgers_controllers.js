accedd50808a30d6bbc40a716c44eee36784a702
Yazan Deek <ydeek91@yahoo.com>
2020-10-08 21:01:25 -0400

    created the router for the app.

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -2,3 +2,17 @@
 var express = require("express");
 // Import the model to use its db functions for burger.js
 var burger = require("../models/burger.js");
+
+
+// Create the router for the app, and export the router at the end of your file.
+var router = express.Router();
+// Create routes and set up logic where required.
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
