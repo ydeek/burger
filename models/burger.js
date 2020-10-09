1954fa50fd01421de11fbd54b6c97d401d1f022a
Yazan Deek <ydeek91@yahoo.com>
2020-10-08 20:53:39 -0400

    created the code that will call the orm functions.

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,17 @@
 // Import orm.js into burger.js
 var orm = require("../config/orm.js");
+
+// The code that will call the ORM functions using burger specific input for the ORM.
+var burger = {
+    // Display all burgers in the db.
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+    // Add a new burger to the db.
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
