48d19a5c8aa3b027c6aa7aa6b62327acb3e2fd12
Yazan Deek <ydeek91@yahoo.com>
2020-10-08 21:34:19 -0400

    parse and json the application

--- server.js
+++ server.js
@@ -8,3 +8,9 @@ var PORT = process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
