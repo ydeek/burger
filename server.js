e2ffdd2923215d32fb73d3f89ed719e6a335f90d
Yazan Deek <ydeek91@yahoo.com>
2020-10-08 21:35:57 -0400

    imported routes and gave the server access to them.

--- server.js
+++ server.js
@@ -20,3 +20,13 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
