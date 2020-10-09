cc1c544ed4a96acfb926631b822da0e712c834d4
Yazan Deek <ydeek91@yahoo.com>
2020-10-08 21:04:17 -0400

    set burger devoured status to true in burger_controllers.js

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -24,3 +24,19 @@ router.post("/api/burgers", function (req, res) {
         res.json({ id: result.insertId });
     });
 });
+
+// Set burger devoured status to true.
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({ devoured: req.body.devoured }, condition, function (result) {
+        if (result.changedRows === 0) {
+            // If no rows were changed, then the ID must not exist, so 404.
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
